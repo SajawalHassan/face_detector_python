7048b2036bdd50cddce16e4153108f15d362785e
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-02 15:05:35 +0500

    Add face detection

--- face_detector.py
+++ face_detector.py
@@ -15,6 +15,13 @@ while True:
     # Make Video grayscale
     grayscaled_vid = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    # Getting coordinates of face
+    face_coordinates = trained_face_data.detectMultiScale(frame)
+
+    # Draw rectangle around face coordinates
+    for (x, y, w, h) in face_coordinates:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (randrange(255), randrange(255), randrange(255)), 10)
+
     # Show vid
-    cv.imshow("Face Decetor app", grayscaled_vid)
+    cv.imshow("Face Decetor app", frame)
     cv.waitKey(150)
